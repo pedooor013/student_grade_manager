0e51e87ee581a19ed77efddca820dc11211dd207
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-08-27 15:47:31 -0300

    addd: criação do metodo POST

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import http from 'http';
+import {v4} from 'uuid';
 //Porta do servidor
 const port = 3000;
 const grades = [
@@ -12,17 +13,33 @@ const grades = [
 //Criação do http
 const server = http.createServer((request, response) =>{
     // funções para configurar a API
-    const {method, url} = request
+    const {method, url} = request;
+
+    let body = '';
+    request.on('data', chunk =>{
+        body += chunck.toString();
+    });
+
+    request.on('end', () =>{
 
     if(url === '/grades' && method === 'GET'){
         response.writeHead(200, {'content-type': 'application/json'});
         response.end(JSON.stringify(grades));
-    }else{
+    }else if(url === '/grades' && method === 'POST'){
+        const { studentName, subject, grade} = JSON.parse(body);
+        grades.push;
+        const newGrade = {id: v4(), studentName, subject, grade};
+        response.writeHead(201, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify(newGrade));
+    }
+    
+    else{
         response.writeHead(404, {'Content-Type': 'application/json'});
         response.end(JSON.stringify({message: 'Route not found'}));
     }
-});
 
+    });
+});
 server.listen(port, ()=>{ //Liga o servidor
     console.log(`Server running in port ${port}`);
 });
